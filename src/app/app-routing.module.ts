f86b306a142f57f672742972bffc71972293f3a9
VladanStar <vladancupric@gmail.com>
2023-03-23 22:11:31 +0100

    Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,11 @@ import { BookDetailsComponent } from './components/book-details/book-details.com
 import { EditBookComponent } from './components/edit-book/edit-book.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  {path: '', redirectTo:'login', pathMatch:'full'},
+  {path: 'login', component : LoginComponent},
   { path: 'book/add', component: AddBookComponent },
   { path: '', component: BookComponent },
   { path: 'book/:id', component: BookDetailsComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { LoginComponent } from './components/login/login.component';
 
 
 
@@ -34,7 +35,8 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     BookDetailsComponent,
     EditBookComponent,
     AddBookComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,

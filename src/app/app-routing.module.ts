65212464b9a1919f1b3faf70d48500cddf190242
VladanStar <vladancupric@gmail.com>
2023-03-23 21:03:07 +0100

    Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,18 @@ import { BookComponent } from './components/book/book.component';
 import { BookDetailsComponent } from './components/book-details/book-details.component';
 import { EditBookComponent } from './components/edit-book/edit-book.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
-  {path:"book/add", component:AddBookComponent},
-  {path:'',component:BookComponent},
-  {path:"book/:id",component:BookDetailsComponent},
-  {path:"book/edit/:id",component:EditBookComponent},
-
+  { path: 'book/add', component: AddBookComponent },
+  { path: '', component: BookComponent },
+  { path: 'book/:id', component: BookDetailsComponent },
+  { path: 'book/edit/:id', component: EditBookComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
